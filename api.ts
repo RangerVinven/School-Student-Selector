29c88da59168555fbfb7bbac377d97492137171d
daniel <daniel.mcpherson@live.co.uk>
2022-04-24 15:32:57 +0100

    Added the invalid credentials response to the /signin endpoint

--- api.ts
+++ api.ts
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post("/signin", (req, res) => {
-    res.send({
-        "token": "fh64lnz"
-    })
+    // Check if the user enters valid credentials
+    if(req.body.email === "test" && req.body.password === "test") {
+        res.send({
+            "token": "fh64lnz"
+        });
+    } else {
+        res.send({
+            "error": "Invalid credentials"
+        })
+    }
+    
 });
 
 app.listen(8000, () => {
