c5506278cd192bf7b54f0d70bcf377787bc459c6
daniel <daniel.mcpherson@live.co.uk>
2022-05-02 19:20:31 +0100

    Added the Chosen Options bit, need to make the clear functional

--- src/components/Wheel.tsx
+++ src/components/Wheel.tsx
@@ -1,33 +1,34 @@
 import React, { useState } from 'react'
 import { Wheel as WinWheel } from 'react-custom-roulette';
 
-export default function Wheel() {
+interface Props {
+    options: { option: string; }[],
+
+    selectedOptions: Array<string>,
+    setSelectedOptions: Function
+}
+export default function Wheel(props: Props) {
     const [mustSpin, setMustSpin] = useState(false);
     const [prizeNumber, setPrizeNumber] = useState(0);
-    const [selectedOptions, setSelectedOptions] = useState<Array<Number>>([]);
-
-    const data = [
-        { option: '0' },
-        { option: '1' },
-        { option: '2' },
-      ];
       
     return (
         <div>
             <WinWheel mustStartSpinning={mustSpin}
             prizeNumber={prizeNumber}
-            data={data}
-            backgroundColors={["#3e3e3e", "df3428", "#df3428"]}
+            data={props.options}
+            backgroundColors={["green", "red", "teal", "violet", "orange"]}
             spinDuration={0.3}
-            onStopSpinning={() => setMustSpin(false)}
+            onStopSpinning={() => {
+                setMustSpin(false);
+
+                //Adds the winningNum to the selectedOptions
+                props.setSelectedOptions([...props.selectedOptions, props.options[prizeNumber].option]);
+            }}
             />
 
             <div className="flex justify-evenly mt-1">
                 <button className="p-1 px-2 text-white  font-bold bg-purple-500 rounded-lg" onClick={() => {
-                    const winningNum = Math.floor(Math.random() * data.length);
-
-                    //Adds the winningNum to the selectedOptions
-                    setSelectedOptions([...selectedOptions, winningNum]);
+                    const winningNum = Math.floor(Math.random() * props.options.length);
 
                     // Makes the wheel spin
                     setPrizeNumber(winningNum);
@@ -35,14 +36,20 @@ export default function Wheel() {
                 }}>Spin</button>
 
                 <button className="p-1 px-2 text-white font-bold bg-purple-500 rounded-lg" onClick={() => {
-                    let winningNum: number = Math.floor(Math.random() * data.length);
-                    
+                    let winningNum: number = Math.floor(Math.random() * props.options.length);
+                    let counter = 0;
+
                     // Makes sure the winningNum is unique
-                    while(selectedOptions.includes(winningNum)) {
-                        winningNum = Math.floor(Math.random() * data.length);
+                    while(props.selectedOptions.includes(props.options[winningNum].option)) {
+                        counter++;
+                        winningNum = Math.floor(Math.random() * props.options.length);
+
+                        // Breaks out of the loop if all the options have been selected
+                        if(counter > props.options.length) {
+                            alert("All options have been selected");
+                            return;
+                        }
                     }
-                    // Adds the winningNum to the selectedOptions
-                    setSelectedOptions([...selectedOptions, winningNum]);
 
                     // Makes the wheel spin
                     setPrizeNumber(winningNum);
--- src/pages/WheelPage.tsx
+++ src/pages/WheelPage.tsx
@@ -1,18 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Wheel from "../components/Wheel";
 import WheelNavbar from '../components/WheelNavbar';
 
-export default function CreateWheel() {
-  return (
-    <div className="flex justify-center h-full">
-        <div className="mt-5 w-5/6">
-            <WheelNavbar />
-            <br />
-            <div className="flex justify-center items-center h-full">
-                <Wheel />
-            </div>
+export default function WheelPage() {
+	const [options, setOptions] = useState([
+		{ option: "Daniel" },
+        { option: "Patrick" },
+        { option: "Anya" },
+        { option: "Mum" },
+        { option: "Dad" }
+	]);
+	const [selectedOptions, setSelectedOptions] = useState<Array<string>>([]);
 
-        </div>
-    </div>
-  )
+	return (
+		<div className="">
+			<div className="mt-5">
+				<WheelNavbar />
+			</div>
+			<br />
+				
+			<div className="flex justify-center h-full w-full">
+				<div className="w-1/3"></div>
+				<div className="w-1/3">
+					<Wheel options={options} selectedOptions={selectedOptions} setSelectedOptions={setSelectedOptions} />
+				</div>
+				<div className="w-1/3 mr-5 ml-20">
+					<div className="flex flex-col">
+						<div className="flex justify-between items-center mb-3">
+							<h1 className="text-lg font-bold">Chosen Options</h1>
+							<button className="p-1 px-1 bg-red-500 rounded-lg" onClick={() => {
+								// Clears the selected options
+								setSelectedOptions([]);	
+							}}>Clear All</button>
+						</div>
+
+						{
+							selectedOptions.map(option => {
+								return (
+									<>
+										<div className="flex justify-between items-center">
+											<h1 className="text-lg">{option}</h1>
+											<button className="p-1 px-1 bg-red-500 rounded-lg">Clear</button>
+										</div>
+										<hr className="mt-1 mb-2" />
+									</>
+								)
+							})
+						}
+
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
